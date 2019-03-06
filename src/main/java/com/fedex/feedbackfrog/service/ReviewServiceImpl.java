c2a0befc1a4fc4d0e5fcba6204e96f00f2ef8683
Bea Huszár <husz.beata@gmail.com>
2019-03-06 14:54:20 +0100

    add: review service

--- src/main/java/com/fedex/feedbackfrog/service/ReviewServiceImpl.java
+++ src/main/java/com/fedex/feedbackfrog/service/ReviewServiceImpl.java
@@ -1,37 +1,52 @@
 package com.fedex.feedbackfrog.service;
 
 import com.fedex.feedbackfrog.model.dto.ReviewDTO;
+import com.fedex.feedbackfrog.model.entity.Review;
 import com.fedex.feedbackfrog.repository.ReviewRepository;
+import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
 public class ReviewServiceImpl implements ReviewService {
 
   private ReviewRepository repository;
+  private ModelMapper mapper;
 
-  public ReviewServiceImpl(ReviewRepository repository) {
+  public ReviewServiceImpl(ReviewRepository repository, ModelMapper mapper) {
     this.repository = repository;
+    this.mapper = mapper;
   }
 
   @Override
   public void saveReview(ReviewDTO reviewDTO) {
-
+    if (reviewDTO != null){
+      repository.save(mapper.map(reviewDTO, Review.class));
+    }
   }
 
   @Override
   public List<ReviewDTO> getAllDtos() {
-    return null;
+    List<ReviewDTO> reviewDTOList = new ArrayList<>();
+    List<Review> reviewList = new ArrayList<>();
+    repository.findAll().forEach(reviewList::add);
+
+    for (Review review : reviewList) {
+     reviewDTOList.add(mapper.map(review, ReviewDTO.class));
+    }
+
+    return reviewDTOList;
   }
 
   @Override
   public ReviewDTO getById(long id) {
-    return null;
+    return mapper.map(repository.findById(id), ReviewDTO.class);
   }
 
   @Override
   public void deleteById(long id) {
-
+    repository.deleteById(id);
   }
 }
