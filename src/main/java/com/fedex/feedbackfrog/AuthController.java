6576b60882c3541c7a04ec6c28a60d28db8ed106
Andor Korosi <4nd0rkorosi@gmail.com>
2019-03-06 22:41:19 +0100

    refactor and new env

--- src/main/java/com/fedex/feedbackfrog/AuthController.java
+++ src/main/java/com/fedex/feedbackfrog/AuthController.java
@@ -1,6 +1,6 @@
 package com.fedex.feedbackfrog;
 
-import org.springframework.beans.factory.annotation.Autowired;
+
 import org.springframework.security.oauth2.provider.OAuth2Authentication;
 import org.springframework.web.bind.annotation.*;
 
@@ -9,11 +9,6 @@ import java.util.LinkedHashMap;
 
 @RestController
 public class AuthController {
-
-  @Autowired
-  private AuthService authService;
-
-  public static String json = " ";
 
   @RequestMapping(value="/user")
   public Principal user(Principal principal) {
