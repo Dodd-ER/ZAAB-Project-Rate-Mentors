590b7565ab232668ad97c3de27b2cea508da52f7
Bea Huszár <husz.beata@gmail.com>
2019-03-06 16:04:02 +0100

    upd: reviewController

--- src/main/java/com/fedex/feedbackfrog/controller/ReviewController.java
+++ src/main/java/com/fedex/feedbackfrog/controller/ReviewController.java
@@ -5,7 +5,6 @@ import com.fedex.feedbackfrog.service.ReviewService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -24,7 +23,12 @@ public class ReviewController {
     return new ResponseEntity<>(service.getById(id), HttpStatus.OK);
   }
 
-  @DeleteMapping("/delete/{id}")
+  @GetMapping
+  public ResponseEntity getAllReviews(){
+    return new ResponseEntity<>(service.getAllDtos(), HttpStatus.OK);
+  }
+
+  @DeleteMapping("/{id}")
   public ResponseEntity deleteByID(@PathVariable long id){
     service.deleteById(id);
     return new ResponseEntity<>(HttpStatus.OK);
@@ -35,5 +39,4 @@ public class ReviewController {
     service.saveReview(reviewDTO);
     return new ResponseEntity<>(HttpStatus.CREATED);
   }
-
 }
