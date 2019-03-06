20441b8c3480b189254d8f29ad900f53bd945e26
Bea Huszár <husz.beata@gmail.com>
2019-03-06 10:09:13 +0100

    initial commit

---
+++ src/main/java/com/fedex/feedbackfrog/FeedbackfrogApplication.java
@@ -0,0 +1,13 @@
+package com.fedex.feedbackfrog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FeedbackfrogApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(FeedbackfrogApplication.class, args);
+  }
+
+}
