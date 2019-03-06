525b2577971c9674b2e77c5556d55bf68b9e2f99
Andor Korosi <4nd0rkorosi@gmail.com>
2019-03-06 21:15:59 +0100

    :art: get principal details

--- src/main/java/com/fedex/feedbackfrog/AuthController.java
+++ src/main/java/com/fedex/feedbackfrog/AuthController.java
@@ -1,18 +1,37 @@
 package com.fedex.feedbackfrog;
 
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+
+import com.sun.deploy.net.HttpResponse;
+import org.json.JSONObject;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.web.JsonPath;
+import org.springframework.http.HttpRequest;
+import org.springframework.security.oauth2.provider.OAuth2Authentication;
+import org.springframework.web.bind.annotation.*;
 
 import java.security.Principal;
+import java.util.HashMap;
+import java.util.LinkedHashMap;
 
 @RestController
 public class AuthController {
 
+  @Autowired
+  private AuthService authService;
+
+  public static String json = " ";
+
   @RequestMapping(value="/user")
   public Principal user(Principal principal) {
-    String asd = principal.toString();
+    LinkedHashMap asd = new LinkedHashMap();
+    asd = (LinkedHashMap) ((OAuth2Authentication) principal).getUserAuthentication().getDetails();
     System.out.println(asd);
+    System.out.println(principal.toString());
+
     return principal;
   }
+
+
+
 }
---
+++ src/main/java/com/fedex/feedbackfrog/AuthService.java
@@ -0,0 +1,9 @@
+package com.fedex.feedbackfrog;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class AuthService {
+
+
+}
