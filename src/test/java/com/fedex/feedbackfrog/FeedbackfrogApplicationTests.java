c4fdd41058f59117f473ba6de5a9c981d64593d5
Dodó <m.zoltan17@gmail.com>
2019-03-07 10:58:34 +0100

    for merging

--- src/test/java/com/fedex/feedbackfrog/FeedbackfrogApplicationTests.java
+++ src/test/java/com/fedex/feedbackfrog/FeedbackfrogApplicationTests.java
@@ -1,15 +1,15 @@
-//package com.fedex.feedbackfrog;
-//
-//import org.junit.Test;
-//import org.junit.runner.RunWith;
-//import org.springframework.boot.test.context.SpringBootTest;
-//import org.springframework.test.context.junit4.SpringRunner;
-//
-//@RunWith(SpringRunner.class)
-//@SpringBootTest
-//public class FeedbackfrogApplicationTests {
-//
-//  @Test
-//  public void contextLoads() {
-//  }
-//}
+package com.fedex.feedbackfrog;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class FeedbackfrogApplicationTests {
+
+  @Test
+  public void contextLoads() {
+  }
+}
