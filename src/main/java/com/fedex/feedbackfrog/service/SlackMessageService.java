ddd0752643cc7caade7a9df1ee177d284722a52b
Bea Huszár <husz.beata@gmail.com>
2019-03-28 19:24:01 +0100

    upd: refactor SlackMessageService

--- src/main/java/com/fedex/feedbackfrog/service/SlackMessageService.java
+++ src/main/java/com/fedex/feedbackfrog/service/SlackMessageService.java
@@ -14,15 +14,26 @@ import java.util.Arrays;
 
 @Service
 public class SlackMessageService {
+
   @Value("${SLACKTOKEN}")
   private String slackToken;
 
   public void sendMessage(ReviewDTO_Post dto) throws GeneralException {
+    HttpEntity<String> entity = new HttpEntity<String>(getMessageBodyAsJson(dto), getMessageHeader());
+    RestTemplate restTemplate = new RestTemplate();
+    restTemplate.exchange("https://slack.com/api/chat.postMessage", HttpMethod.POST, entity, SlackMessage.class);
+  }
+
+  private HttpHeaders getMessageHeader(){
     HttpHeaders header = new HttpHeaders();
     header.setAccept(Arrays.asList(MediaType.APPLICATION_JSON));
     header.setContentType(MediaType.APPLICATION_JSON);
     header.set("Authorization", "Bearer " + slackToken);
-    ObjectMapper mapper = new ObjectMapper();
+
+    return header;
+  }
+
+  private SlackMessage getMessageBody(ReviewDTO_Post dto){
     SlackMessage message = new SlackMessage(dto.mentor.slackAlias, Arrays.asList(
         new Block(new SlackText("*You have a new rating*: " + getEmoji(dto.rating.toString()))),
         new BlockWithImage(
@@ -31,15 +42,21 @@ public class SlackMessageService {
                 getImageUrl(dto.rating.toString()),
                 "frog"))
     ));
+
+    return message;
+  }
+
+  private String getMessageBodyAsJson(ReviewDTO_Post dto) throws GeneralException{
+    ObjectMapper mapper = new ObjectMapper();
+
     String jsonString = null;
     try {
-      jsonString = mapper.writeValueAsString(message);
+      jsonString = mapper.writeValueAsString(getMessageBody(dto));
     } catch (JsonProcessingException e) {
       throw new GeneralException(e.getMessage(), HttpStatus.BAD_REQUEST);
     }
-    HttpEntity<String> entity = new HttpEntity<String>(jsonString, header);
-    RestTemplate restTemplate = new RestTemplate();
-    restTemplate.exchange("https://slack.com/api/chat.postMessage", HttpMethod.POST, entity, SlackMessage.class);
+
+    return jsonString;
   }
 
   private String getImageUrl(String rating) {
